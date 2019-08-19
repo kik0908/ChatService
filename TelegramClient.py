50d8e0729b1b24f617b7da131154593cf76b654b
dr3al <47189738+dr3al@users.noreply.github.com>
2019-08-19 16:21:02 +0300

    Some upgrade.
    Additional functions.

--- TelegramClient.py
+++ TelegramClient.py
@@ -1,16 +1,39 @@
-from pyrogram import Client, Filters
-from pyrogram.api.functions.messages.edit_chat_admin import EditChatAdmin
-
+from pyrogram import Client
 
 class TelegramAPI():
     def __init__(self, client):
         self.app = client
 
+    def chat_premission(self, chat_id):
+        class chatPremission:
+            def __init__(
+                self,
+                send_message: bool = True,
+                send_media_messages: bool = True,
+                send_other_messages: bool = True,
+                add_web_page_previews: bool = True,
+                send_polls: bool = True,
+                change_info: bool = False,
+                invite_users: bool = False,
+                pin_messages: bool = False
+                ):
+                self.can_send_messages = send_message
+                self.can_send_media_messages = send_media_messages
+                self.can_send_other_messages = send_other_messages
+                self.can_add_web_page_previews = add_web_page_previews
+                self.can_send_polls = send_polls
+                self.can_change_info = change_info
+                self.can_invite_users = invite_users
+                self.can_pin_messages = pin_messages
+        return self.app.set_chat_permissions(chat_id, chatPremission())
+
     def send_message(self, id, message):
         return self.app.send_message(id, message)
 
     def create_chat(self, title, users):
-        return self.app.create_group(title, users)
+        chat_info = self.app.create_group(title, users)
+        t.chat_premission(chat_info['id'])
+        return chat_info
 
     def add_members(self, chat_id, users):
         return self.app.add_chat_members(chat_id, users)
@@ -29,9 +52,9 @@ class TelegramAPI():
 
 
 if __name__ == "__main__":
-    api_id = "956114"
-    api_hash = "fb3e9d5d801a823983d487640bcb8f55"
+    api_id = "token"
+    api_hash = "token"
 
-    lastChatID = None
     with Client('session', api_id, api_hash) as client:
         t = TelegramAPI(client)
+        t.function()
