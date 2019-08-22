c65cf130cc84efead491bce3c15d961a18b76b63
kik0908 <kik0908@yandex.ru>
2019-08-22 11:46:46 +0300

    Addition REST api

--- server.py
+++ server.py
@@ -22,9 +22,11 @@ telegram_client = None
 @ns.route('/')
 class Chats(flask_restplus.Resource):
     createChatModelReq = api.model("Create chat",
-                                   {'title': fields.String(min_length=1, max_length=40, description="Название чата"),
-                                    'users': fields.List(fields.String,
-                                                         description='Можно передать в строке логины или id участников строго в формате String')})
+                                   {'title': fields.String(min_length=1, max_length=128, description="Название чата"),
+                                    'users': fields.List(fields.String(min_length=1),
+                                                         description='Можно передать в строке логины или id участников строго в формате String'),
+                                    'description': fields.String(min_length=1, max_length=255,
+                                                                 description='Описание группы')})
     createChatModelAns = api.model("Create chat answer", {'chat_id': fields.Integer(), 'title': fields.String()})
 
     @api.response(201, 'Чат успешно создан')
@@ -36,14 +38,14 @@ class Chats(flask_restplus.Resource):
         global telegram_client
 
         data = request.json
-        ans = telegram_client.create_chat(data['title'], data['users'])
+        ans = telegram_client.create_chat(data['title'], data['description'], data['users'])
         return {'chat_id': ans['id'], 'title': data['title']}, 201
 
 
 @ns.route('/-<int:chat_id>/users/<string:user_id>/')
 class ChatsUsers(flask_restplus.Resource):
     @api.response(201, "Пользователь добавлен")
-    # @utils.error_handler_for_http_answer
+    @utils.error_handler_for_http_answer
     def post(self, chat_id, user_id):
         """Добавление пользователя в чат"""
         global telegram_client
@@ -52,9 +54,16 @@ class ChatsUsers(flask_restplus.Resource):
         except:
             user_id = user_id
 
-        print(user_id, type(user_id))
-        ans = telegram_client.add_members(0 - chat_id, user_id)
+        ans = telegram_client.add_members(0 - chat_id,
+                                          user_id)  # 0 - chat_id потому что у чатов отрицательные id, а в функцию передается положительное id (>0)
         return ans, 201
+
+    @api.response(204, "Пользователь удален")
+    @utils.error_handler_for_http_answer
+    def delete(self, chat_id, user_id):
+        """Удаление пользователя из чата"""
+        ans = telegram_client.kick_members(0 - chat_id, user_id)
+        return True, 204
 
 
 @ns.route('/-<int:chat_id>/message/')
@@ -76,10 +85,31 @@ class ChatMessage(flask_restplus.Resource):
         return {'chat_id': ans.chat.id, 'message_id': ans.message_id}, 201
 
 
+@ns.route('/-<int:chat_id>/pin/')
+class FixingChatMessage(flask_restplus.Resource):
+    """Закрепление или открепление сообщения"""
+    pinMessageReq = api.model("Pin message", {'message_id': fields.Integer()})
+
+    @api.response(200, 'Сообщение было закреплено')
+    @api.expect(pinMessageReq)
+    @utils.error_handler_for_http_answer
+    def post(self, chat_id):
+        """Закрепление сообщения"""
+        data = request.json
+        ans = telegram_client.pin_message(0 - chat_id, data['message_id'])
+        return ans, 200
+
+    @api.response(204, "Сообщение было откреплено")
+    @utils.error_handler_for_http_answer
+    def delete(self, chat_id):
+        telegram_client.unpin_message(0 - chat_id)
+        return True, 204
+
+
 @ns.route("/-<int:chat_id>/admins/<string:user_id>")
 class ChatAdmins(flask_restplus.Resource):
     @api.response(201, "Админ добавлен")
-    # @utils.error_handler_for_http_answer
+    @utils.error_handler_for_http_answer
     def post(self, chat_id, user_id):
         """Добавление админ в чат"""
         global telegram_client
